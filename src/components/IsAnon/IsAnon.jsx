8d5d9290b291fbebbef4cab1ee959aadf6ce3722
waggnerl <wagnermflira@gmail.com>
2023-03-14 17:00:54 +0000

    adjust_redirect_when_loggedin

--- src/components/IsAnon/IsAnon.jsx
+++ src/components/IsAnon/IsAnon.jsx
@@ -5,13 +5,19 @@ import Loading from "../Loading/Loading";
 
 function IsAnon({ children }) {
   const { isLoggedIn, isLoading } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
   // If the authentication is still loading ⏳
   if (isLoading) {
     return <Loading />;
   }
 
-  if (isLoggedIn) {
+  if (isLoggedIn && user.role === "personal") {
+    // If the user is logged in, navigate to home page ❌
+    return <Navigate to="/list-students" />;
+  }
+
+  if (isLoggedIn && user.role === "student") {
     // If the user is logged in, navigate to home page ❌
     return <Navigate to="/" />;
   }

7a9061c4f7c9d5f16c22b20c3424c48011e9726a
waggnerl <wagnermflira@gmail.com>
2023-03-16 22:45:44 +0000

    feat:Change at main

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,11 +3,15 @@ import backgroundImage from "../../assets/gym.jpg";
 
 function HomePage() {
   return (
-    <div style={{ backgroundImage: `url(${backgroundImage})` }}
-    className="bg-cover bg-center bg-no-repeat min-h-screen  bg-gray-100 flex flex-col justify-center sm:py-12">
-      <h1 className="text-white text-4xl font-bold border-solid border-black border-2">Transform your workout
-       into a unique journey with our gym app, where expert personal trainers and a 
-       community of students come together to offer personalized and motivating workout routines.</h1>
+    <div
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+      className="bg-cover bg-center bg-no-repeat min-h-screen  bg-gray-100 flex flex-col justify-center sm:py-12"
+    >
+      <h1 className="text-white text-4xl font-bold border-solid bg-black mx-6 rounded-xl p-6 border-black border-2">
+        Transform your workout into a unique journey with our gym app, where
+        expert personal trainers and a community of students come together to
+        offer personalized and motivating workout routines.
+      </h1>
     </div>
   );
 }

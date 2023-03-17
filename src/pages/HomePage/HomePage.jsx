12418353b74fed647def0617eb3f63c714fa0e93
waggnerl <wagnermflira@gmail.com>
2023-03-17 00:17:20 +0000

    feat:Change padding top at home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,7 +5,7 @@ function HomePage() {
   return (
     <div
       style={{ backgroundImage: `url(${backgroundImage})` }}
-      className="bg-cover bg-center min-h-screen   bg-gray-100 flex flex-col justify-start pt-32 sm:justify-center sm:py-12"
+      className="bg-cover bg-center min-h-screen   bg-gray-100 flex flex-col justify-start pt-36 sm:justify-center sm:py-12"
     >
       <h1 className="text-white text-3xl sm:text-4xl font-bold border-solid bg-black mx-6 rounded-xl p-6 border-black border-2">
         Transform your workout into a unique journey with our gym app, where
